4aedf4f5ff5b50535de07d97478e471de92a3775
Abhijeet <thakurabhi1110@gmail.com>
2022-06-26 22:38:44 +0530

    Practice Add

--- src/com/practice/Intro/Main.java
+++ src/com/practice/Intro/Main.java
@@ -1,10 +1,7 @@
-package com.abhi;
+package com.practice.Intro;
 
 public class Main {
-
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
-
-
 }
