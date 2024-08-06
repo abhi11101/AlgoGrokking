2a92ca6104beab044e0d9204e0f37b638a0bf92d
Abhijeet <thakurabhi1110@gmail.com>
2024-08-06 21:59:52 +0530

    Array Extra

---
+++ src/com/CodeHelp/C1_Arrays/Extra/E4_PowerOfTwo_LEETCODE.java
@@ -0,0 +1,33 @@
+package com.CodeHelp.C1_Arrays.Extra;
+
+/*
+https://leetcode.com/problems/power-of-two/description/
+ */
+public class E4_PowerOfTwo_LEETCODE {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPowerOfTwo(8));
+        System.out.println(isPowerOfTwo(128));
+    }
+
+    public static boolean isPowerOfTwo(int n) {
+
+        return n>0 && (n&(n-1)) == 0;
+    }
+
+    public static boolean isPowerOfTwo2(int n) {
+
+        if (n==0) return false;
+        int count=0;
+        while (n>0){
+            if ((n&1)==1)count++;
+            n>>=1;
+
+        }
+        if (count==1)return true;
+        else return false;
+
+    }
+
+}
---
+++ src/com/CodeHelp/C1_Arrays/Extra/E5_NegativeOneSide.java
@@ -0,0 +1,69 @@
+package com.CodeHelp.C1_Arrays.Extra;
+
+public class E5_NegativeOneSide {
+
+    public static void main(String[] args) {
+
+        int[] data = { 1,-4,6,98,-3,-21,12,11,-86};
+        printArray(data);
+//        negativeOneSide(data);
+        anotherWay(data);
+    }
+
+    public static void negativeOneSide(int[] data) {
+
+        int start =0;
+        int end = data.length-1;
+        int index =0;
+
+        while(index < end) {
+
+            if (data[index]<0){
+
+                int temp = data[index];
+                data[index] = data[start];
+                data[start] = temp;
+                index++;
+                start++;
+            }else {
+                int temp = data[index];
+                data[index] = data[end];
+                data[end] = temp;
+                end--;
+
+            }
+        }
+        printArray(data);
+
+    }
+
+    public static void anotherWay(int[] data) {
+
+        int j =0;
+        for(int i=0;i<data.length;i++){
+
+            if (data[i]<0){
+
+                int temp = data[i];
+                data[i] = data[j];
+                data[j] = temp;
+                j++;
+
+            }
+
+        }
+        printArray(data);
+
+    }
+
+
+    private static void printArray(int[] arr) {
+
+        for (int num: arr){
+            System.out.print(num + " ");
+        }
+        System.out.println();
+
+    }
+
+}
