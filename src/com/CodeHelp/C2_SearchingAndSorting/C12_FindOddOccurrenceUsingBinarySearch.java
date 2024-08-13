e72e29748dd10490dba588aaadf55de7e96fceea
Abhijeet <thakurabhi1110@gmail.com>
2024-08-13 19:34:21 +0530

    Searching and Sorting Class 2

--- src/com/CodeHelp/C2_SearchingAndSorting/C12_FindOddOccurrenceUsingBinarySearch.java
+++ src/com/CodeHelp/C2_SearchingAndSorting/C12_FindOddOccurrenceUsingBinarySearch.java
@@ -6,7 +6,8 @@ public class C12_FindOddOccurrenceUsingBinarySearch {
 
         int[] nums = {1,1,2,2,3,3,4,4,3,600,600,4,4,};
         int[] data = {5,4,4};
-        System.out.println(search(nums));
+        int[] num2 = {1, 1, 2, 2, 3, 3, 4, 4, 5};
+        System.out.println(search(num2));
 
     }
 
@@ -16,25 +17,30 @@ public class C12_FindOddOccurrenceUsingBinarySearch {
         int end = nums.length - 1;
         int mid;
 
-        while (start <= end){
+        while (start <= end) {
+            if (start==end)break;
 
             mid = start + (end - start) / 2;
 
-            if (mid %2 ==1){
-                mid = mid-1;
+            if (mid%2==0){
+
+                if (nums[mid]==nums[mid+1]){
+                    start=mid+2;
+                }else {
+                    end=mid;
+                }
+
+            }else {
+
+                if (nums[mid]==nums[mid-1]){
+                    start=mid+1;
+                }else {
+                    end=mid-1;
+                }
             }
 
-            if (nums[mid] == nums[mid + 1]){
-
-                start = mid+2;
-
-            }
-            else {
-                end = mid - 1;
-            }
 
         }
-
         return nums[start];
     }
 
