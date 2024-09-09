810197f5c113736e714d6a66eb7e87dcedc44246
Abhijeet <thakurabhi1110@gmail.com>
2024-09-09 09:00:23 +0530

    Update

---
+++ src/com/CodeHelp/Extra/E59_MaximumAverageSubarray_LEETCODE.java
@@ -0,0 +1,45 @@
+package com.CodeHelp.Extra;
+
+/*
+https://leetcode.com/problems/maximum-average-subarray-i/description/
+ */
+public class E59_MaximumAverageSubarray_LEETCODE {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,12,-5,-6,50,3};
+        System.out.println(findMaxAverage(arr,4));
+
+    }
+
+    public static double findMaxAverage(int[] nums, int k) {
+
+        int index = 0;
+
+        double sum = 0;
+        double avg = 0;
+        double max = 0;
+
+        while (index<k){
+            sum+=nums[index];
+            index++;
+        }
+        avg =  sum/k;
+        if (avg>max)max=avg;
+
+        while (index<nums.length){
+
+            int num = nums[index];
+            sum+=num;
+
+            int oldNum = nums[index-k];
+            sum-=oldNum;
+
+            avg = sum/k;
+            if (avg>max)max=avg;
+            index++;
+        }
+        return max;
+    }
+
+}
---
+++ src/com/CodeHelp/Extra/E61_MaxNumberOfKSumPairs_LEETCODE.java
@@ -0,0 +1,36 @@
+package com.CodeHelp.Extra;
+
+import java.util.Arrays;
+
+/*
+https://leetcode.com/problems/max-number-of-k-sum-pairs/description/
+ */
+public class E61_MaxNumberOfKSumPairs_LEETCODE {
+
+    public static void main(String[] args) {
+
+        int[] arr = {1,2,3,4};
+        System.out.println(maxOperations(arr,5));
+
+    }
+
+    public static int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+
+        int i =0;
+        int j = nums.length-1;
+        int count =0;
+        while(i<j){
+            int sum = nums[j]+nums[i];
+            if(sum==k){
+                count++;
+                i++;
+                j--;
+            }
+            else if(sum>k)j--;
+            else i++;
+        }
+        return count;
+    }
+
+}
