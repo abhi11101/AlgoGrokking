fe593dfc4b7438c063abc911783741643c6ab9f3
Abhijeet <thakurabhi1110@gmail.com>
2024-10-03 16:59:09 +0530

    Heap Class 1

--- src/com/CodeHelp/C13_Heap/C4_HeapSort.java
+++ src/com/CodeHelp/C13_Heap/C4_HeapSort.java
@@ -17,6 +17,19 @@ public class C4_HeapSort {
         heapSort1Based(arr2,arr2.length);
         printArray(arr2);
 
+
+        int[] arr3 = {12,15,13,11,14};
+
+        printArray(arr3);
+        buildHeap0Based(arr3,arr3.length);
+        heapSort0Based(arr3,arr3.length);
+        printArray(arr3);
+
+        int[] arr4 = {12,56,43,6,78,87,5,44,3,23};
+        printArray(arr4);
+        buildHeap0Based(arr4,arr4.length);
+        heapSort0Based(arr4,arr4.length);
+        printArray(arr4);
     }
 
 
@@ -59,6 +72,49 @@ public class C4_HeapSort {
         for (int i = (n-1)/2;i>0;i--){
             heapify1Based(arr,n,i);
         }
+    }
+
+    public static void heapSort0Based(int[] arr, int n) {
+
+        while (n!=0){
+            int temp = arr[0];
+            arr[0] = arr[n-1];
+            arr[n-1] = temp;
+            n--;
+
+            heapify0Based(arr,n,0);
+        }
+
+    }
+
+    public static void heapify0Based(int[] arr, int n, int index) {
+
+        int left = 2*index+1;
+        int right = 2*index+2;
+
+        int largest = index;
+
+        if (left<n && arr[left]>arr[largest]) largest = left;
+        if(right<n && arr[right]>arr[largest]) largest = right;
+
+        if (largest != index) {
+
+            int temp = arr[index];
+            arr[index] = arr[largest];
+            arr[largest] = temp;
+
+            index = largest;
+            heapify0Based(arr,n,index);
+        }
+    }
+
+    public static void buildHeap0Based(int[] arr, int n){
+
+        for (int i = (n/2)-1;i>=0;i--){
+            heapify0Based(arr,n,i);
+        }
+
+
     }
 
     public static void printArray(int[] arr){
